5a799971fc099ba6964b352ba230d2eb8855b444
niravshah <nirav.shah83@gmail.com>
2016-05-09 00:27:10 +0100

    Starting with a Spring Boot Initializr Skeleton

---
+++ src/main/java/com/nns/wd/WdWebcrawlerApplication.java
@@ -0,0 +1,12 @@
+package com.nns.wd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WdWebcrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WdWebcrawlerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/nns/wd/WdWebcrawlerApplicationTests.java
@@ -0,0 +1,18 @@
+package com.nns.wd;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = WdWebcrawlerApplication.class)
+@WebAppConfiguration
+public class WdWebcrawlerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
